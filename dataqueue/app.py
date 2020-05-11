b06df0fde17566d55cdbb7d49663f2bde73afaec
Krzysztof Kułak <krzysztofpiotrkulak@gmail.com>
2020-05-11 22:57:26 +0200

    add api-keys, fix dockerfile and add docker-compose file

--- dataqueue/app.py
+++ dataqueue/app.py
@@ -1,4 +1,5 @@
 import datetime
+import os
 
 from flask import Flask, request
 
@@ -6,10 +7,14 @@ from .queues import get_queue
 from .settings import queue_topics_map
 
 app = Flask(__name__)
+app.config["INTAKE_API_KEY"] = os.environ["INTAKE_API_KEY"]
+app.config["OUTTAKE_API_KEY"] = os.environ["OUTTAKE_API_KEY"]
 
 
 @app.route('/intake', methods=["POST"])
 def intake():
+    if request.headers.get("x-api-key") != app.config["INTAKE_API_KEY"]:
+        return "api key incorrect", 401
     input_json = request.json
     input_json["TIMESTAMP"] = datetime.datetime.now()
     queues = assign_queues(input_json["topic"])
@@ -20,14 +25,16 @@ def intake():
 
 @app.route('/outtake/<string:queue>', methods=["GET"])
 def outtake(queue):
+    if request.headers.get("x-api-key") != app.config["OUTTAKE_API_KEY"]:
+        return "api key incorrect", 401
     try:
         response = get_queue(queue).pop()
     except ValueError:
-        return f"Queue {queue} not found", 404
+        return f"queue {queue} not found", 404
     if response:
         return response
     else:
-        return f"Queue {queue} empty", 404
+        return f"queue {queue} empty", 404
 
 
 def assign_queues(topic):
